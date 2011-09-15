8c169f5ac17ddfab2b9e6709568e42669f31b35b
Sam Phippen <samphippen@googlemail.com>
2011-09-15 23:46:12 +0100

    use redis's built in monitoring for the monitor script
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- monitor.py
+++ monitor.py
@@ -4,10 +4,7 @@ import sys
 if __name__ == "__main__":
   r = redis.Redis(host='localhost', port=6379, db=0)
   ps = r.pubsub()
-  ps.psubscribe("org.srobo*")
-  last_time = 0
-  last_offset = 0
-  last_start = 0
-  lg = ps.listen()
-  for x in lg:
-    print x["channel"], r.get(x["channel"])
+  ps.execute_command("monitor")
+  while True:
+    print ps.parse_response()
+
