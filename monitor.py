0bedfb382bed5fa64d29d3cf1b570a3700620ac6
Sam Phippen <samphippen@googlemail.com>
2011-10-01 17:22:28 +0100

    repurposed monitor.py to do channel publishing
    when a variable gets updated it is necessary to inform people
    that are subscribed to that variable that it has been updated
    this script now handles that, instead of having scripts
    responsible for doing their own notifications
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- monitor.py
+++ monitor.py
@@ -2,9 +2,12 @@ import redis
 import sys
 
 if __name__ == "__main__":
-  r = redis.Redis(host='localhost', port=6379, db=0)
-  ps = r.pubsub()
-  ps.execute_command("monitor")
-  while True:
-    print ps.parse_response()
-
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    ps = r.pubsub()
+    ps.execute_command("monitor")
+    while True:
+        resp = ps.parse_response()
+        parts = resp.split(" ")
+        if len(parts) >= 4 and parts[1] == "\"SET\"":
+            r.publish(parts[2], "")
+            print "publishing:", parts[2]
