69846e0644c229b22ee6d14df9dc8d43a387dbb1
Sam Phippen <samphippen@googlemail.com>
2012-01-11 18:57:39 +0000

    Added a function to get the current match
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nextmatch.py
+++ nextmatch.py
@@ -4,7 +4,17 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 
 def get_next_match():
     global r;
-    match = r.lindex("org.srobo.matches", 0).split(",")
+    index = int(r.get("org.srobo.matches.currmatch"))+1
+    return match_from_ms(r.lindex("org.srobo.matches", index))
+
+
+def get_current_match():
+    global r;
+    index = int(r.get("org.srobo.matches.currmatch"))
+    return match_from_ms(r.lindex("org.srobo.matches", index))
+
+def match_from_ms(ms):
+    match = ms.split(",")
     match_dict = {"time": float(match[0]),
                   "teams": [ int(x) for x in match[1:]]}
     return match_dict
