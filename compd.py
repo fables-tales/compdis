a2b1090084030d3de58d74e0229737c9f8b8be22
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-02-24 01:02:31 +0000

    Add the use of a function to deal with 'normal' commands
    The command list is now a dictionary giving the number of keys it operates
    on (-1 for unrestricted) and which function to call
    Happily publishes with 'normal' multiple key commands

--- compd.py
+++ compd.py
@@ -5,6 +5,15 @@ import socket, re, shlex
 HOST = "localhost"
 PORT = 6379
 
+def normal(cmd, args):
+	if cmd is -1:
+		cmd = len(args)
+	for i in range(cmd):
+		s.sendall('PUBLISH {0} UPDATE\r\n'.format(args[i]))
+
+def special(cmd, *args):
+	pass
+
 def get_parts(data):
     lexer = shlex.shlex(data, posix=True)
     lexer.whitespace_split = True
@@ -16,54 +25,58 @@ def pub_all():
 def move():
 	print('Moving')
 
-red_mod_coms = ['APPEND',
-		'BLPOP',
-		'BRPOP',
-		'BRPOPLPUSH',
-		'DECR',
-		'DECRBY',
-		'DEL',
-		'FLUSHALL',
-		'FLUSHDB',
-		'HDEL',
-		'HINCRBY',
-		'HMSET',
-		'HSET',
-		'HSETNX',
-		'INCR',
-		'INCRBY',
-		'LINSERT',
-		'LPOP',
-		'LPUSH',
-		'LPUSHX',
-		'LREM',
-		'LSET',
-		'LTRIM',
-		'MSET',
-		'MSETNX',
-		'RPOP',
-		'RPOPLPUSH',
-		'RPUSH',
-		'RPUSHX',
-		'SADD',
-		'SDIFFSTORE',
-		'SET',
-		'SETBIT',
-		'SETEX',
-		'SETNX',
-		'SETRANGE',
-		'SINTERSET',
-		'SORT',
-		'SPOP',
-		'SREM',
-		'SUNIONSTORE',
-		'ZADD',
-		'ZINCRBY',
-		'ZINTERSTORE',
-		'ZREM',
-		'ZREMRANGEBYRANK',
-		'ZREMRANGEBYSCORE',
-		'ZUNIONSTORE'	]
+red_mod_coms = {'APPEND':(1,normal),
+		'BLPOP':(-1,normal),
+		'BRPOP':(-1,normal),
+		'BRPOPLPUSH':(2,normal),
+		'DECR':(1,normal),
+		'DECRBY':(1,normal),
+		'DEL':(-1,normal),
+		'FLUSHALL':(0,special),
+		'FLUSHDB':(0,special),
+		'GETSET':(1,normal),
+		'HDEL':(1,normal),
+		'HINCRBY':(1,normal),
+		'HMSET':(1,normal),
+		'HSET':(1,normal),
+		'HSETNX':(1,normal),
+		'INCR':(1,normal),
+		'INCRBY':(1,normal),
+		'LINSERT':(1,normal),
+		'LPOP':(1,normal),
+		'LPUSH':(1,normal),
+		'LPUSHX':(1,normal),
+		'LREM':(1,normal),
+		'LSET':(1,normal),
+		'LTRIM':(1,normal),
+		'MSET':(-1,special),
+		'MSETNX':(-1,special),
+		'RENAME':(2,special),
+		'RENAMENX':(2,special),
+		'RPOP':(1,normal),
+		'RPOPLPUSH':(2,normal),
+		'RPUSH':(1,normal),
+		'RPUSHX':(1,normal),
+		'SADD':(1,normal),
+		'SDIFFSTORE':(1,normal),
+		'SET':(1,normal),
+		'SETBIT':(1,normal),
+		'SETEX':(1,normal),
+		'SETNX':(1,normal),
+		'SETRANGE':(1,normal),
+		'SINTERSTORE':(1,normal),
+		'SMOVE':(2,normal),
+		'SORT':(1,normal),
+		'SPOP':(1,normal),
+		'SREM':(1,normal),
+		'SUNIONSTORE':(1,normal),
+		'ZADD':(1,normal),
+		'ZINCRBY':(1,normal),
+		'ZINTERSTORE':(1,normal),
+		'ZREM':(1,normal),
+		'ZREMRANGEBYRANK':(1,normal),
+		'ZREMRANGEBYSCORE':(1,normal),
+		'ZUNIONSTORE':(1,normal)	}
 
 red_spec[	'MOVE':move(),
 		'RENAME':move(),
@@ -85,6 +98,5 @@ while True:
 	if parts is None:
 		continue
 	if parts[1].upper() in red_mod_coms:
-		for i in range(red_mod_coms)
-		s.sendall('PUBLISH {0} UPDATE\r\n'.format(parts[2]))
+		red_mod_coms[parts[1].upper()][1](red_mod_coms[parts[1].upper()][0],parts[2:])
 
