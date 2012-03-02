6e5a8d1aba4fc66a4ff94e095baabcd13386f1ab
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-03-02 18:04:51 +0000

    Correct use of some redis functions and add some defined display states

--- displays.py
+++ displays.py
@@ -8,21 +8,28 @@ DB = 0
 
 BASE = 'org.srobo'
 
+STATE_REG = 'register'
+STATE_NOE = 'No Entry'
+STATE_LEA = 'League'
+STATE_MAT = 'Match'
+STATE_SCO = 'Score'
+
 actor = redis.Redis(host=HOST, port=PORT, db=DB)
 subscriber = actor.pubsub()
 
 def setup():
 	print 'Setting up'
-	actor.lpush('{0} displays'.format(BASE))
-	actor.lpush('{0}.displays count locations'.format(BASE))
-	actor.set('{0}.displays.count 0'.format(BASE))
-	actor.lpush('{0}.displays.locations FL FR BL BR DR'.format(BASE))
-	subscriber.psubscribe('{0}.displays.*'.format(BASE))
+	actor.lpush('{0}'.format(BASE),'displays')
+	actor.lpush('{0}.displays'.format(BASE), 'count', 'locations')
+	actor.set('{0}.displays.count'.format(BASE), '0')
+	actor.lpush('{0}.displays.locations'.format(BASE),'FL','FR','BL','BR','DR')
 	print 'Setup complete'
 
 def new_screen(count):
-	actor.lpush('{0}.displays screen{1}'.format(BASE,count))
-	actor.lpush('{0}.displays.screen{1} state team score'.format(BASE,count))
+	print 'New Screen {0}'.format(count)
+	actor.lpush('{0}.displays'.format(BASE),'screen{0}'.format(count))
+	actor.lpush('{0}.displays.screen{1}'.format(BASE,count),'state','team','score')
+	actor.set('{0}.displays.screen{1}.state', STATE_REG)
 
 setup()
 
