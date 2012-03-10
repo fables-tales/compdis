31cc53c7ef89c9015446062caa6bc124de4f88d7
Sam Phippen <samphippen@googlemail.com>
2012-03-10 11:59:02 +0000

    Made next match figure out the current and matches correctly
    this is now based off competition time instead of other computations.
    The current match is only current if it is actually happening right now
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nextmatch.py
+++ nextmatch.py
@@ -1,17 +1,32 @@
 import redis
 
+match_length = 180
+
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 def get_next_match():
     global r;
-    index = int(r.get("org.srobo.matches.currmatch"))+1
-    return match_from_ms(r.lindex("org.srobo.matches", index))
+    len = r.llen("org.srobo.matches") 
+    comptime = float(r.get("org.srobo.time.competition"))
+    for i in xrange(0, len):
+        v = r.lindex("org.srobo.matches", i)
+        match = match_from_ms(v)
+        if match["time"] > comptime+match_length:
+            return match
 
+    return match_from_ms(r.lindex("org.srobo.matches", index))
 
 def get_current_match():
     global r;
-    index = int(r.get("org.srobo.matches.currmatch"))
-    return match_from_ms(r.lindex("org.srobo.matches", index))
+    len = r.llen("org.srobo.matches") 
+    comptime = float(r.get("org.srobo.time.competition"))
+    for i in xrange(0, len):
+        v = r.lindex("org.srobo.matches", i)
+        match = match_from_ms(v)
+        upper = match["time"] + match_length
+        lower = match["time"]
+        if comptime > lower and comptime < upper:
+            return match
 
 def match_from_ms(ms):
     match = ms.split(",")
@@ -21,7 +36,7 @@ def match_from_ms(ms):
     return match_dict
 
 if __name__ == "__main__":
-    time = r.get("org.srobo.time")
+    time = r.get("org.srobo.time.competition")
     print time
-    print get_next_match()
     print get_current_match()
+    print get_next_match()
