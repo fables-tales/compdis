0fba1a027462ad1cfed0e53730dc80cc24515b4d
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-03-25 00:49:12 +0000

    Add a scores script
    Currently:
    	Prints required input format
    	Requests score entry
    	Reads in scores
    	Checks for compatability of entered scores

---
+++ scores.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+
+import redis, shlex
+
+HOST = "localhost"
+PORT = 6379
+DB = 0
+
+actor = redis.Redis(host=HOST, port=PORT, db=DB)
+
+print("Please enter match data (space separated) in the following order:")
+print("Match, Zone, Robot Tokens, Zone Tokens, Bucket Tokens, Bucket count")
+
+def get_parts(data):
+	lexer = shlex.shlex(data, posix=True)
+	lexer.whitespace_split = True
+	return tuple(lexer)
+
+while True:
+	str = raw_input("Score: ")
+	try:
+		p = get_parts(str)
+		if not len(p) is 6:
+			print("Incorrect number of values entered. Please try again")
+			continue
+		for i in range(len(p)):
+			int(p[i])
+		print(p)
+	except ValueError:
+		print("Sorry, incorrectly entered. Please try again")
