0643b810b9fb0dc66325b9d611ab1ca26511b077
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-03-27 18:07:45 +0100

    Setup the displays
    Involves creating a list of the types of zones that want setting, in order
    to allow for easier alteration for different events

--- displays.py
+++ displays.py
@@ -8,12 +8,15 @@ DB = 0
 
 BASE = 'org.srobo'
 
+types = ['zone0', 'zone1', 'zone2', 'zone3', 'door']
 
 actor = redis.Redis(host=HOST, port=PORT, db=DB)
 
 def setup():
 	print 'Setting up'
 	actor.set('{0}.displays.count'.format(BASE), '0')
+	for i in range(len(types)):
+		actor.zadd('{0}.displays.screens'.format(BASE),types[i],0)
 	print 'Setup complete'
 
 
