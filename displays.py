62d10873ce28b17525a7d93a599aec4389073cbe
Thomas Scarsbrook <scarzybrook@gmail.com>
2012-03-27 18:19:24 +0100

    Add a function to print out the displays locations

--- displays.py
+++ displays.py
@@ -19,6 +19,24 @@ def setup():
 		actor.zadd('{0}.displays.screens'.format(BASE),types[i],0)
 	print 'Setup complete'
 
+def print_screens():
+	for i in range(len(types)):
+		score = actor.zscore('{0}.displays.screens'.format(BASE),types[i])
+		if score == 0 or score == None:
+			score = 'Not set'
+		print('{0}:\t{1}'.format(types[i], score))
+	scr = actor.zrange('{0}.displays.screens'.format(BASE),0,-1,False,True)
+	if len(scr) == len(types):
+		raw_input()
+		return
+	for i in range(len(scr)):
+		str = scr[i][1]
+		if scr[i][0] in types:
+			continue
+		if scr[i][1] == 0 or scr[i][0] == None:
+			str = 'Not set'
+		print('{0}:\t{1}'.format(scr[i][0],str))
+	raw_input()
 
 setup()
 
