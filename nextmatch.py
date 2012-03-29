9f7ab06ff213f3b6ec4f81b119775f4982afcc5c
Sam Phippen <samphippen@googlemail.com>
2012-03-28 17:40:34 -0700

    Made nextmatch write data to redis
    the next and current match, as well as the state are now written back to
    redis. This is done by subscribing to the competition time channel and
    pushing out updates whenever the competition time changes
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nextmatch.py
+++ nextmatch.py
@@ -5,6 +5,7 @@ STATE_ENTER = 0x01
 STATE_BOOT  = 0x02
 STATE_LIVE  = 0x04
 STATE_SCORE = 0x08
+import time
 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
@@ -39,7 +40,6 @@ def get_current_state():
     comptime = float(r.get("org.srobo.time.competition"))
     into_match = comptime-current_match["time"]
     minute = into_match/60
-    print minute
     if minute <= 1:
         return STATE_ENTER
     elif minute > 1 and minute <= 2:
@@ -57,9 +57,23 @@ def match_from_ms(ms):
     match_dict["number"] = match[-1]
     return match_dict
 
+def match_to_ms(match):
+    str_teams = []
+    teams = match["teams"]
+    for x in teams:
+        str_teams.append(str(x))
+    return str(match["time"]) + "," + ",".join(str_teams) + "," + str(match["number"])
+
 if __name__ == "__main__":
-    time = r.get("org.srobo.time.competition")
-    print time
-    print get_current_match()
-    print get_next_match()
-    print get_current_state()
+    ps = r.pubsub()
+    ps.psubscribe("org.srobo.time.competition") 
+    lg = ps.listen()
+    for message in lg:
+        ctime = r.get("org.srobo.time.competition")
+        print ctime
+        print get_current_match()
+        print get_next_match()
+        print get_current_state()
+        r.set("org.srobo.matches.current", match_to_ms(get_current_match()))
+        r.set("org.srobo.matches.next", match_to_ms(get_next_match()))
+        r.set("org.srobo.matches.state", get_current_state())
