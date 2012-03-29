9cf3ec75d1dfae6fc4534af24d4bece9a557fc0f
Sam Phippen <samphippen@googlemail.com>
2012-03-28 17:25:46 -0700

    Added states to the nextmatch script
    the get_current_state function will now give you the state the
    competition is currently in by checking how minutes into the current
    match cycle we are
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nextmatch.py
+++ nextmatch.py
@@ -1,6 +1,11 @@
 import redis
 
 match_length = 60*7 
+STATE_ENTER = 0x01
+STATE_BOOT  = 0x02
+STATE_LIVE  = 0x04
+STATE_SCORE = 0x08
+
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -28,6 +33,23 @@ def get_current_match():
         if comptime > lower and comptime < upper:
             return match
 
+def get_current_state():
+    global r
+    current_match = get_current_match()
+    comptime = float(r.get("org.srobo.time.competition"))
+    into_match = comptime-current_match["time"]
+    minute = into_match/60
+    print minute
+    if minute <= 1:
+        return STATE_ENTER
+    elif minute > 1 and minute <= 2:
+        return STATE_BOOT
+    elif minute > 2 and minute <= 5:
+        return STATE_LIVE
+    elif minute > 5 and minute <= 7:
+        return STATE_SCORE
+
+    
 def match_from_ms(ms):
     match = ms.split(",")
     match_dict = {"time": float(match[0]),
@@ -40,3 +62,4 @@ if __name__ == "__main__":
     print time
     print get_current_match()
     print get_next_match()
+    print get_current_state()
